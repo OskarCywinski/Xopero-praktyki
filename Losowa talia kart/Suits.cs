46a7f2f4be509a7128f40ce147f50dea2a1ab8e0
Oskar <kontakt.oskarcywinski@gmail.com>
2022-02-07 14:44:09 +0100

    Losowa talia kart / numery koszulek

---
+++ Losowa talia kart/Suits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Losowa_talia_kart
+{ 
+    enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts,
+    }
+}
---
+++ Numery koszulek sławnych zawodników/JerseyNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Numery_koszulek_sławnych_zawodników
+{
+    class JerseyNumber
+    {
+        public string Player { get; private set; }
+        public int YearRetired { get; private set; }
+
+        public JerseyNumber(string player,int numberRetired)
+        {
+            Player = player;
+            YearRetired = numberRetired;
+        }
+    }
+}
