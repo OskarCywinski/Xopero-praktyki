71449d4fda9ba7df9861f953ac58beeaa024dbdf
OskarCywinski <kontakt.oskarcywinski@gmail.com>
2022-02-23 13:15:32 +0100

    Kompresja i szyfrowanie

---
+++ Kompresja+szyfrowanie/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Kompresja_szyfrowanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.szyfruj = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.odszyfruj = new System.Windows.Forms.Button();
+            this.Dekompresuj = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 129);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(296, 309);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(314, 129);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(296, 309);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nazwa Pliku";
+            // 
+            // szyfruj
+            // 
+            this.szyfruj.Location = new System.Drawing.Point(12, 59);
+            this.szyfruj.Name = "szyfruj";
+            this.szyfruj.Size = new System.Drawing.Size(53, 23);
+            this.szyfruj.TabIndex = 5;
+            this.szyfruj.Text = "Szyfruj";
+            this.szyfruj.UseVisualStyleBackColor = true;
+            this.szyfruj.Click += new System.EventHandler(this.szyfruj_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Zawartość pliku";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Kompresuj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(441, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Wynik";
+            // 
+            // odszyfruj
+            // 
+            this.odszyfruj.Location = new System.Drawing.Point(71, 59);
+            this.odszyfruj.Name = "odszyfruj";
+            this.odszyfruj.Size = new System.Drawing.Size(65, 23);
+            this.odszyfruj.TabIndex = 9;
+            this.odszyfruj.Text = "Odszyfruj";
+            this.odszyfruj.UseVisualStyleBackColor = true;
+            this.odszyfruj.Click += new System.EventHandler(this.odszyfruj_Click);
+            // 
+            // Dekompresuj
+            // 
+            this.Dekompresuj.Location = new System.Drawing.Point(220, 59);
+            this.Dekompresuj.Name = "Dekompresuj";
+            this.Dekompresuj.Size = new System.Drawing.Size(88, 23);
+            this.Dekompresuj.TabIndex = 10;
+            this.Dekompresuj.Text = "Dekompresuj";
+            this.Dekompresuj.UseVisualStyleBackColor = true;
+            this.Dekompresuj.Click += new System.EventHandler(this.Dekompresuj_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Szyfruj i kompresuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(142, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Zdekompresuj i odszyfruj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Dekompresuj);
+            this.Controls.Add(this.odszyfruj);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.szyfruj);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button szyfruj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button odszyfruj;
+        private System.Windows.Forms.Button Dekompresuj;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Kompresja+szyfrowanie/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Kompresja_szyfrowanie
+{
+    public partial class Form1 : Form
+    {
+
+        string file,text;
+ 
+  
+        public byte[] CompressBF(string input)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            using (MemoryStream output = new MemoryStream())
+            {
+                using (DeflateStream compressor = new DeflateStream(output, CompressionMode.Compress))
+                {
+                    compressor.Write(bytes, 0, bytes.Length);
+                }
+                return output.ToArray();
+            }
+
+        }
+        public string DecompressBF(byte[] data)
+        {
+            using (MemoryStream input = new MemoryStream(data))
+            {
+                using (DeflateStream decompressor = new DeflateStream(input, CompressionMode.Decompress))
+                {
+                    using (StreamReader reader = new StreamReader(decompressor))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+        public string Encryptxx(string text)
+        {
+
+            byte[] src = Encoding.UTF8.GetBytes(text);
+            byte[] key = Encoding.ASCII.GetBytes("0123456789abcdef");
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.Mode = CipherMode.ECB;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.KeySize = 128;
+
+            using (ICryptoTransform encrypt = aes.CreateEncryptor(key, null))
+            {
+                byte[] dest = encrypt.TransformFinalBlock(src, 0, src.Length);
+                encrypt.Dispose();
+                return Convert.ToBase64String(dest);
+            }
+        }
+
+        public string Decryptxx(string text)
+        {
+            byte[] src = Convert.FromBase64String(text);
+            RijndaelManaged aes = new RijndaelManaged();
+            byte[] key = Encoding.ASCII.GetBytes("0123456789abcdef");
+            aes.KeySize = 128;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.ECB;
+            using (ICryptoTransform decrypt = aes.CreateDecryptor(key, null))
+            {
+                byte[] dest = decrypt.TransformFinalBlock(src, 0, src.Length);
+                decrypt.Dispose();
+                return Encoding.UTF8.GetString(dest);
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void szyfruj_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = Encryptxx(text);
+            
+        }
+
+        private void odszyfruj_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = Decryptxx(text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] compressed = CompressBF(textBox3.Text);
+
+            using (FileStream s = File.Create(Application.StartupPath+$@"\{file}.bfcp"))
+            {
+                s.Write(compressed);
+                textBox4.Text = Encoding.UTF8.GetString(compressed); 
+            }
+
+        }
+
+        private void Dekompresuj_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = DecompressBF(File.ReadAllBytes(Application.StartupPath+$@"\{file}"));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string encrypted = Encryptxx(text);
+            byte[] compressed = CompressBF(Encryptxx(text));
+
+            using (FileStream s = File.Create(Application.StartupPath + $@"\compressed{file}.bfcp"))
+            {
+                s.Write(compressed);
+            }
+                textBox4.Text = "Tekst zaszyfrowany: \r\n " + encrypted + "\r\n" + "Tekst skompresowany: \r\n" + Encoding.UTF8.GetString(compressed);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           string decompress =  DecompressBF(File.ReadAllBytes(Application.StartupPath + $@"\{file}"));
+           string decrypt = Decryptxx(decompress);
+           textBox4.Text = "Tekst zdekompresowany: \r\n" + decompress + "\r\n" + "Tekst odszyfrowany: \r\n" + decrypt;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            file = textBox1.Text;
+            text = File.ReadAllText(Application.StartupPath+$@"\{file}");
+            textBox3.Text = text;
+        }
+    }
+}
